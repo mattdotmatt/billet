0d39a3a2fc6bff35ebfe5efd3e628cf57bfbae61
Matt Young <matt@fansz.com>
2016-01-02 23:18:49 +0000

    Merge remote ignore

--- Billet/obj/Release/Package/PackageTmp/Scripts/error.jsx
+++
@@ -1,8 +0,0 @@
-﻿var React = require('react');
-
-module.exports = React.createClass({
-
-    render: function () {
-        return <div className="alert alert-warning" role="alert">{this.props.errorMsg}</div>
-    }
-});
